b3f70ab304e2489116fe7fe2db57923d3c838307
quynhethereal <49337640+quynhethereal@users.noreply.github.com>
2021-05-14 13:28:16 +0700

    fix front end check for forms

--- src/main/resources/templates/login.js
+++ src/main/resources/templates/login.js
@@ -72,12 +72,4 @@ $(function() {
         });
     });
 
-    $(".my-login-validation").submit(function() {
-        var form = $(this);
-        if (form[0].checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-        form.addClass('was-validated');
-    });
 });
