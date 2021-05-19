993d7624f345bc035778279bdc024da03267a3b5
quynhethereal <49337640+quynhethereal@users.noreply.github.com>
2021-05-19 17:12:17 +0700

    change home

--- src/main/java/com/ecommerce/backend/repository/MyOrderRepository.java
+++ src/main/java/com/ecommerce/backend/repository/MyOrderRepository.java
@@ -2,10 +2,12 @@ package com.ecommerce.backend.repository;
 
 import com.ecommerce.backend.entity.Order;
 import com.ecommerce.backend.entity.Product;
+import com.ecommerce.backend.entity.Store;
 import com.ecommerce.backend.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -15,13 +17,15 @@ public class MyOrderRepository {
     @Autowired
     private OrderRepository orderRepository;
 
-    public Order save(Order order){return orderRepository.save(order);}
-    public void delete(Order order){orderRepository.delete(order);}
-    public Order findByUserAndProduct(User user, Product product){return orderRepository.findByUserAndProduct(user, product);}
-    public boolean existByUserAndProduct(User user, Product product){return orderRepository.existsByUserAndProduct(user, product);}
-    public List<Order> findByUser(User user){return orderRepository.findByUser(user);}
-    public Order findById(long id){
-        return orderRepository.findById(id).orElseThrow(() -> new NoSuchElementException());
+    public List<Order> basketItemGet(long userId, String orderTime){
+        return orderRepository.findByUser_IdAndTime(userId, orderTime);
     }
-    public List<Order> findAll(){return orderRepository.findAll();}
+
+    public Order save(Order order) {
+        return orderRepository.save(order);
+    }
+
+    public void delete(Order order){
+        orderRepository.delete(order);
+    }
 }
