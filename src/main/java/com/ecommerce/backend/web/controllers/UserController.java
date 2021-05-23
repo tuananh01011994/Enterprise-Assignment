459023b4fbe94bca898abb031dc30e04fc4761c3
quynhethereal <49337640+quynhethereal@users.noreply.github.com>
2021-05-23 11:55:40 +0700

    change

--- src/main/java/com/ecommerce/backend/web/controllers/UserController.java
+++ src/main/java/com/ecommerce/backend/web/controllers/UserController.java
@@ -64,13 +64,13 @@ public class UserController {
     public ResponseEntity<Map<String, String>> checkoutBasket(@Valid @RequestBody List<Order> orderList){
 //        System.out.println(orderList.get(0));
         Map<String,String> map = new HashMap<>();
-        Order orderIn = new Order();
 //
 //        if (orderList.isEmpty()){
 //            map.put("message","Empty basket");
 //            return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
 //        }
         for (Order order : orderList){
+            Order orderIn = new Order();
             orderIn.setTime(order.getTime());
             orderIn.setQuantity(order.getQuantity());
             orderIn.setUser(myUserRepository.findByID(order.getUser().getId()));
